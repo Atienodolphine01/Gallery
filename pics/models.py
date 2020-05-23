3d70072d2a07f327ecb075cb122cfd14489ba45a
Dolphine Atieno <awuordolphine65@gmail.com>
2020-05-23 10:02:41 +0300

    created two classes,location and category

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    place = models.CharField(max_length=255) #input location
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.place
+
+
+class Category(models,Model):
+    name = models.CharField(max_length=255)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+ 
+
+    
