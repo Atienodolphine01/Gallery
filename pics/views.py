889939bcf7aabd95d22d3be3caa151aaabcd2822
Dolphine Atieno <awuordolphine65@gmail.com>
2020-05-24 18:09:23 +0300

    defined searched results and paths

--- pics/views.py
+++ pics/views.py
@@ -5,3 +5,13 @@ from .models import Picture
 def index(request):
     pictures = Picture.objects.all()
     return render(request,'index.html',{"pictures":pictures})
+
+def search_results(request):
+    if 'picture' in request.GET and request.GET["picture"]:
+        search_term = request.GET.get("picture")
+        searched_Pictures = Picture.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message": message,"Pictures": searched_Pictures})
+
+    
